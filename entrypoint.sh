86ae778f7bd6953375539fdaecccdeef8debe507
mehmetakkan <mamosakkan@gmail.com>
2024-06-30 21:09:21 +0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Export environment variables from .env file
-export $(cat .env | xargs)
+export $(grep -v '^#' .env | xargs)
 
 # Run the application
 exec java -jar /app/NoteSharingApp.jar
