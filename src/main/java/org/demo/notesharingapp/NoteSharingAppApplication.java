f518c3d7339b721b6ab7a3840497c53030424321
mehmetakkan <mamosakkan@gmail.com>
2024-06-30 17:56:41 +0300

    Configure environment variables loading: Add dotenv support and update application configuration

--- src/main/java/org/demo/notesharingapp/NoteSharingAppApplication.java
+++ src/main/java/org/demo/notesharingapp/NoteSharingAppApplication.java
@@ -1,13 +1,38 @@
 package org.demo.notesharingapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteSharingAppApplication {
+public class NoteSharingAppApplication implements CommandLineRunner {
+
+    private static Dotenv dotenv = Dotenv.load();
+
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
 
     public static void main(String[] args) {
+        // Set environment variables
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(NoteSharingAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("DB_URL: " + dbUrl);
+        System.out.println("DB_USERNAME: " + dbUsername);
+        System.out.println("DB_PASSWORD: " + dbPassword);
+    }
 }
